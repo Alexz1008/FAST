a842805a3bfa104663aeddab90db386657f0c21e
Alex Z <alexz1012@yahoo.com>
2018-10-30 15:24:42 -0700

    Alex-2: Start css work on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Header.css';
 
 // The Header creates links that can be used to navigate
 // between routes.
 const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/profile'>Profile</Link></li>
-        <li><Link to='/mylistings'>My Listings</Link></li>
-      </ul>
-    </nav>
-  </header>
+  <div className="navbar">
+    <div className="navbar-button">
+      <div className="navbar-text"><Link to='/'>Home</Link></div>
+    </div>
+    <div className="navbar-button">
+      <div className="navbar-text"><Link to='/profile'>Profile</Link></div>
+    </div>
+    <div className="navbar-button">
+      <div className="navbar-text"><Link to='/mylistings'>My Listings</Link></div>
+      </div>
+  </div>
 )
 
 export default Header
