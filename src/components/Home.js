186d9379a27b62c23146d2611f26fb1712c7acbc
Alex Z <alexz1012@yahoo.com>
2018-10-30 10:40:31 -0700

    Added components folder and starter files

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => (
+  <div>
+    Goodass homepage
+  </div>
+)
+
+export default Home
---
+++ src/components/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profile = () => (
+  <div>
+    Goodass profile
+  </div>
+)
+
+export default Profile
