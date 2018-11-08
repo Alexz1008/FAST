1f50e2cc8fed68db164617d6354775491a4a0b89
Alex Z <alexz1012@yahoo.com>
2018-11-08 14:30:58 -0800

    Alex-4: Added primitive listings

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Header from '../Header/header'
 import Sidebar from '../Sidebar/sidebar'
+import './home.css'
+
+var i;
+var listings = [];
+const numbers = [1, 2, 3, 4, 5];
+
+const getListings = numbers.map((number) =>
+  <div className="listings">{number}</div>
+);
 
 const Home = () => (
   <div>
     <Header />
-    <Sidebar />
+    <div className="content">
+      <Sidebar />
+      {getListings}
+    </div>
   </div>
 )
 
