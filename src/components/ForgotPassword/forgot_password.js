c3b1cc6f3a99ec95efe8b91bce49a8acadd20471
bcrosta <crosta.brayden@gmail.com>
2018-11-17 09:00:41 -0800

    Brayden-3: Updated login, register, forgot-password html+css

--- src/components/ForgotPassword/forgot_password.js
+++ src/components/ForgotPassword/forgot_password.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import './forgot_password.css'
 
 const Home = () => (
-  <div className="forgot-password-form">
-    Gimme that fuckin email<br />
-    UCSD Email:<br />
-    <input type="text" name="email" />
-    <br /><br />
-    <button>Send Password Reset</button>
+  <div className="forgot-password-form" class="center">
+    <h1><a href="/login" >Triton Market</a></h1>
+    <div class="content-box">
+      <h3>Forgot password</h3>
+
+      <label for="email"><strong>UCSD Email:</strong></label> <br /> 
+      <input type="email" name="email" id="email" /> <br />
+
+      <br />
+
+      <button type="submit" id="forgot-password">Reset Password</button>
+
+    </div>
   </div>
 )
 
