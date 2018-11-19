3accce713f82cf213a295fb06f898eca3c9ceb97
Alex Z <alexz1012@yahoo.com>
2018-11-18 20:21:08 -0800

    Alex: More pretty listings

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -7,7 +7,7 @@ const listingid = [0, 1, 2, 3, 4, 5];
 const listingtitles = ["Singular Banana", "Single in La Jolla Palms", "iClicker", "AP CS Textbook", "Physics Textbook", "Couch"];
 const images = ['https://i5.walmartimages.ca/images/Large/580/6_r/875806_R.jpg',
                 'https://cdn.discordapp.com/attachments/431923743028412427/513601584748560384/image0.jpg',
-'https://images-na.ssl-images-amazon.com/images/I/31TZyxgL38L._SX255_BO1,204,203,200_.jpg',
+'https://uwaterloo.ca/centre-for-teaching-excellence/sites/ca.centre-for-teaching-excellence/files/styles/sidebar-220px-wide/public/iclicker.png?itok=J1P1LRte',
 'https://www.pearsonhighered.com/assets/bigcovers/0/1/3/1/0131374699.jpg',
 'http://cuhsphysics.weebly.com/uploads/3/8/9/5/38955313/1484413_orig.png',
 'https://target.scene7.com/is/image/Target/GUEST_3954d54d-41c2-4b87-8929-d60d47a574e6'];
@@ -18,11 +18,12 @@ const listingdescriptions = ["Cheap singular banana, I wanted to eat it but now 
 "Good condition, but has highlighting in it. About 20% cheaper than market price.",
 "I regret choosing physics as my major because now I cannot be in Gary's CSE 110 class.",
 "Comfy couch that I don't need anymore. Barely any wear and tear, looks just like new."];
+const myListings = [true, false, false, false, false, true]
 const saveStates = [false, true, false];
 
 const getListings = listingid.map((id) =>
   <div className="listing">
-    <Listing title={listingtitles[id]} image={images[id]} price={listingprice[id]} desc={listingdescriptions[id]} id={id} saved={saveStates[id]} />
+    <Listing title={listingtitles[id]} image={images[id]} price={listingprice[id]} desc={listingdescriptions[id]} id={id} saved={saveStates[id]} isMyListing={myListings[id]} />
   </div>
 );
 
--- src/components/Listing/listing.js
+++ src/components/Listing/listing.js
@@ -8,13 +8,14 @@ export class Listing extends React.Component {
     this.handleRemoveClickSaved = this.handleRemoveClickSaved.bind(this);
     this.handleAddClickConfirm = this.handleAddClickConfirm.bind(this);
     this.handleRemoveClickConfirm = this.handleRemoveClickConfirm.bind(this);
+    this.handleAddInterestClick = this.handleAddInterestClick.bind(this);
+    this.handleRemoveInterestClick = this.handleRemoveInterestClick.bind(this);
     this.state = {title: this.props.title, image: this.props.image, price: this.props.price, desc: this.props.desc, id: this.props.id,
                   isInterested: this.props.isInterested, saved: this.props.saved, confirmed: this.props.confirmed, isMyListing: this.props.isMyListing,
                   }
   }
 
   handleAddClickSaved() {
-    console.log(this.props);
     this.setState({saved: true});
   }
 
@@ -25,19 +26,19 @@ export class Listing extends React.Component {
     console.log(this.props);
     this.setState({confirmed: true});
   }
-
   handleRemoveClickConfirm() {
     this.setState({confirmed: false});
   }
-
-  handleDeleteTagClick() {
-    console.log("delete tag(s)");
+  handleAddInterestClick() {
+    this.setState({isInterested: true});
   }
-
+  handleRemoveInterestClick() {
+    this.setState({isInterested: false});
+  }
   handleDeleteListingClick() {
     console.log("delete listing");
   }
-  handleInterestClick() {
+  handleEditListingClick() {
     console.log("delete listing");
   }
   render() {
@@ -45,7 +46,7 @@ export class Listing extends React.Component {
     const isMyListing= this.state.isMyListing;
     const isConfirmed = this.state.confirmed;
     return (
-	  <div className="listing-container">
+	  <div className={isMyListing ? 'listing-container-self' : 'listing-container'}>
       <div className="listing-upper">
         <div className="listing-title"><b>{this.state.title}</b></div>
         <img className="listing-picture" src={this.state.image} alt="did not load" />
@@ -57,34 +58,39 @@ export class Listing extends React.Component {
         </center>
       </div>
       <div className="listing-lower">
-        <button className="listing-button" id="saveButton" onClick={this.state.saved ? this.handleRemoveClickSaved : this.handleAddClickSaved}>
-          {this.state.saved ? 'Saved' : 'Save'}
-        </button>
         <div>
             {isMyListing ?
               console.log("invalid interest listing")
             : 
-            <button className="listing-button" id="interestedListing" onclick={this.handleInterestClick()}>
+            <button className={this.state.saved ? 'listing-button-selected' : 'listing-button-unselected'} id="saveButton" onClick={this.state.saved ? this.handleRemoveClickSaved : this.handleAddClickSaved}>
+              {this.state.saved ? 'Saved' : 'Save'}
+            </button>}
+        </div>
+        <div>
+            {isMyListing ?
+              console.log("invalid interest listing")
+            : 
+            <button className={isInterested ? 'listing-button-selected' : 'listing-button-unselected'} id="interestedListing" onClick={this.state.isInterested ? this.handleRemoveInterestClick : this.handleAddInterestClick}>
               {isInterested ? 'Interested' : 'Show Interest'}
             </button>}
         </div>
         <div>
             {isMyListing ?
-              <button className="listing-button" id="deleteListing" onclick={this.handleDeleteListingClick()}>
+              <button className='listing-button-unselected' id="deleteListing" onclick={this.handleDeleteListingClick()}>
                 Delete
               </button>
             : console.log("invalid delete listing")}
         </div>
         <div>
           {isMyListing ?
-              <button className="listing-button" id="editListing" onclick={this.handleEditListingClick()}>
+              <button className='listing-button-unselected' id="editListing" onclick={this.handleEditListingClick()}>
                 Edit
               </button>
               : console.log("invalid edit listing")}
         </div>
         <div>
           {isInterested ?
-            <button className="listing-button" id="confirmTransaction" onClick={this.state.confirmed ? this.handleRemoveClickConfirm : this.handleAddClickConfirm}>
+            <button className={this.state.confirmed ? 'listing-button-selected' : 'listing-button-unselected'} id="confirmTransaction" onClick={this.state.confirmed ? this.handleRemoveClickConfirm : this.handleAddClickConfirm}>
               {this.state.confirmed ? 'Cancel Transaction' : 'Confirm Transaction'}
             </button>
             : console.log("invalid interested listing")}
