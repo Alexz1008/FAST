6a57aacaf7c0d5cd508a15718c42c92c0372938b
toh006 <33336293+toh006@users.noreply.github.com>
2018-11-18 22:28:06 -0800

    Update my_listings.js

--- src/components/MyListings/my_listings.js
+++ src/components/MyListings/my_listings.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from '../Header/header'
-import MyListingsSidebar from '../MyListingsSidebar/my_listings_sidebar'
 import './my_listings.css'
 import { Listing } from '../Listing/listing'
 const listingid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
