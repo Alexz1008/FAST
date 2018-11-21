1d56bcef00552b57138180fefd2b16873561b5c4
BuildTools <unconfigured@null.spigotmc.org>
2018-11-21 14:50:14 -0800

    Alex: Fixed bugs with syntax and added background for Gary

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -4,27 +4,29 @@ import './login.css'
 export class Login extends React.Component {
   render () {
     return (
-      <div class="center">
-        <form className="login-form">
-        <h1><a href="/login" >Triton Market</a></h1>
-        <div class="content-box">
-          <h3 class="basic-title">Log in</h3>
+      <div className="container">
+        <div className="center">
+          <form className="login-form">
+          <h1><a href="/login" >Triton Market</a></h1>
+          <div className="content-box">
+            <h3 className="basic-title">Log in</h3>
 
-          <label for="email"><strong>UCSD Email:</strong></label> <br /> 
-          <input type="email" class="basic-input" name="email" id="email" required /> <br />
+            <label htmlFor="email"><strong>UCSD Email:</strong></label> <br /> 
+            <input type="email" className="basic-input" name="email" id="email" required /> <br />
 
-          <label for="password"><strong>Password:</strong></label>
-          <a id="login-forgot-pass" href="/forgot_password">Forgot password?</a><br />
-          <input type="password" class="basic-input" name="password" id="password" required /> <br />
+            <label htmlFor="password"><strong>Password:</strong></label>
+            <a id="login-forgot-pass" href="/forgot_password">Forgot password?</a><br />
+            <input type="password" className="basic-input" name="password" id="password" required /> <br />
 
-          <br />
+            <br />
 
-          <button type="submit" class="basic-button" id="login-button" >Log in</button> <br />
+            <button type="submit" className="basic-button" id="login-button" >Log in</button> <br />
 
-          <a href="/register">Register account</a> <br />
+            <a href="/register">Register account</a> <br />
 
+          </div>
+          </form>
         </div>
-        </form>
       </div>
     );
   }
--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -28,50 +28,27 @@ export class Register extends React.Component {
 
   render() {
     return(
-      <div class="center">
-      <form className="register-form">
-        <h1><a href="/login" class="site-title">Triton Market</a></h1>
-        <div class="content-box">
-          <h3 class="basic-title">Register account</h3>
+      <div className="container">
+        <div className="center">
+        <form className="register-form">
+          <h1><a href="/login" className="site-title">Triton Market</a></h1>
+          <div className="content-box">
+            <h3 className="basic-title">Register account</h3>
 
-          <label for="email"><strong>UCSD Email:</strong></label><br />
-          <input type="email" class="basic-input" name="email" id="email" onChange= {e => this.setState({email: e.target.value})} required/> <br />
+            <label htmlFor="email"><strong>UCSD Email:</strong></label><br />
+            <input type="email" className="basic-input" name="email" id="email" onChange= {e => this.setState({email: e.target.value})} required/> <br />
 
-          <label for="password"><strong>Password:</strong></label><br />
-          <input type="password" class="basic-input" name="password" id="password" onChange= {e => this.setState({password: e.target.value})} required/> <br />
+            <label htmlFor="password"><strong>Password:</strong></label><br />
+            <input type="password" className="basic-input" name="password" id="password" onChange= {e => this.setState({password: e.target.value})} required/> <br />
 
-          <label for="repassword"><strong>Re-enter password:</strong></label><br />
-          <input type="password" class="basic-input" name="password" id="repassword" required/><br />
-          <br /><br />
-          <button className="register-form" class="basic-button" type="submit" id="register-button" onClick={this.pushToFirebase.bind(this)}>Register</button>
+            <label htmlFor="repassword"><strong>Re-enter password:</strong></label><br />
+            <input type="password" className="basic-input" name="password" id="repassword" required/><br />
+            <br /><br />
+            <button className="register-form" className="basic-button" type="submit" id="register-button" onClick={this.pushToFirebase.bind(this)}>Register</button>
+          </div>
+          </form>
         </div>
-        </form>
       </div>
     );
   }
 }
-
-
-/* const Register = () => (
-
-  <div class="center">
-  <form className="register-form">
-    <h1><a href="/login" class="site-title">Triton Market</a></h1>
-    <div class="content-box">
-      <h3 class="basic-title">Register account</h3>
-
-      <label for="email"><strong>UCSD Email:</strong></label><br />
-      <input type="email" class="basic-input" name="email" id="email" required/> <br />
-
-      <label for="password"><strong>Password:</strong></label><br />
-      <input type="password" class="basic-input" name="password" id="password" required/> <br />
-
-      <label for="repassword"><strong>Re-enter password:</strong></label><br />
-      <input type="password" class="basic-input" name="password" id="repassword" required/><br />
-      <br /><br />
-      <button className="register-form" class="basic-button" type="submit" id="register-button">Register</button>
-    </div>
-    </form>
-  </div>
-)
-*/
