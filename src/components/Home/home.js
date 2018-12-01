f6053d020fccf6581e23e723e7dae98ce4bf1490
Tony Huang <toh006@ucsd.edu>
2018-12-01 15:13:25 -0800

    CSS for home page / tony

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -61,7 +61,7 @@ export class Home extends React.Component {
             <Sidebar />
           </div>
           <div className="content-listings">
-            {listings.length ? listings : <p> The Marketplace currently has no listings. Come back later or add one yourself? </p>}
+            {listings.length ? listings : <div className = "content-text"> The Marketplace currently has no listings. Come back later or add one yourself? </div>}
           </div>
         </div>
       </div>
