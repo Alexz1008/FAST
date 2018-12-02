6d10baf693e7c5f947bbcdf05a840f8e74597954
Tony Huang <toh006@ucsd.edu>
2018-12-02 14:56:47 -0800

    Changes to messages

--- src/components/Messages/messages.js
+++ src/components/Messages/messages.js
@@ -8,7 +8,7 @@ const messageid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const fromSender = [true, false, false, true, true, false, true, false, true, false]
 const messages = ["Hello?","Hello!","Are you still interested in purchasing this banana?", "Yes I am.", "Where can we meet up?",
 "How about we meet up in front of Geisel at 10 AM tomorrow?", "I can't make 10 AM, can you do 11 AM instead?", "Sure, that's fine!",
-"Great, see you then!", "Yep, see youdfjasld;fjalsdjf;ladjfl;ajd;faj;ldfj;alsdjf;laj;flkasj;ldfjal;sdfj;lasdkjf;lakdjf;lajkdf;lasjdfl;aksjdf;lajdf;lajdf;ljdaf;lasjkdf;alsdkjf;aljdf!"];
+"Great, see you then!", "Yep, see you!"];
 const timestamps = ["3:02:21 PM", "3:02:28 PM", "3:02:48 PM", "3:03:12 PM", "3:03:16 PM", "3:03:44 PM", "3:05:10 PM", "3:05:25 PM",
 "3:05:31 PM", "3:05:34 PM"];
 const recipient = "John";
