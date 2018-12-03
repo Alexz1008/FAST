77041926f549ffe2349bd99e667fd0a6c6b222ce
Tony Huang <toh006@ucsd.edu>
2018-12-03 04:33:01 -0800

    fixed some files

--- src/components/Messages/messages.js
+++ src/components/Messages/messages.js
@@ -6,37 +6,37 @@ import fire from '../Fire/fire'
 import { addToUserList } from '../Utilities/utilities'
 
 
-const messageid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const fromSender = [true, false, false, true, true, false, true, false, true, false]
-const messages = ["Hello?","Hello!","Are you still interested in purchasing this banana?", "Yes I am.", "Where can we meet up?",
-"How about we meet up in front of Geisel at 10 AM tomorrow?", "I can't make 10 AM, can you do 11 AM instead?", "Sure, that's fine!",
-"Great, see you then!", "Yep, see you!"];
-const timestamps = ["3:02:21 PM", "3:02:28 PM", "3:02:48 PM", "3:03:12 PM", "3:03:16 PM", "3:03:44 PM", "3:05:10 PM", "3:05:25 PM",
-"3:05:31 PM", "3:05:34 PM"];
-const recipient = "John";
-const user = "You";
-const getMessages = messageid.map((id) =>
-  <div>
-    {(fromSender[id]) ? (
-      <div className="messages-sent">
-        <div className = "messages-sent-text">
-          {messages[id]}
-        </div>
-        <div className="hover">
-          [{timestamps[id]}]
-        </div>
-      </div>)
-      :
-      (<div className="messages-received">
-        <div className = "messages-received-text">
-        {messages[id]}
-        </div>
-        <div className="hover">
-          [{timestamps[id]}]
-        </div>
-      </div>)}
-  </div>
-);
+// const messageid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const fromSender = [true, false, false, true, true, false, true, false, true, false]
+// const messages = ["Hello?","Hello!","Are you still interested in purchasing this banana?", "Yes I am.", "Where can we meet up?",
+// "How about we meet up in front of Geisel at 10 AM tomorrow?", "I can't make 10 AM, can you do 11 AM instead?", "Sure, that's fine!",
+// "Great, see you then!", "Yep, see you!"];
+// const timestamps = ["3:02:21 PM", "3:02:28 PM", "3:02:48 PM", "3:03:12 PM", "3:03:16 PM", "3:03:44 PM", "3:05:10 PM", "3:05:25 PM",
+// "3:05:31 PM", "3:05:34 PM"];
+// const recipient = "John";
+// const user = "You";
+// const getMessages = messageid.map((id) =>
+  // <div>
+    // {(fromSender[id]) ? (
+      // <div className="messages-sent">
+        // <div className = "messages-sent-text">
+          // {messages[id]}
+        // </div>
+        // <div className="hover">
+          // [{timestamps[id]}]
+        // </div>
+      // </div>)
+      // :
+      // (<div className="messages-received">
+        // <div className = "messages-received-text">
+        // {messages[id]}
+        // </div>
+        // <div className="hover">
+          // [{timestamps[id]}]
+        // </div>
+      // </div>)}
+  // </div>
+// );
 
 export class Messages extends React.Component {
   constructor(props) {
