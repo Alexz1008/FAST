731513dea593f6fa145e535f2a05fff8c42eca37
bcrosta <crosta.brayden@gmail.com>
2018-12-03 00:51:58 -0800

    Brayden: Fixed: accidentally changed whole site's background

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -88,13 +88,13 @@ export class Register extends React.Component {
   // Setup an onDrop method to handle when users drag and drop a file in as their profile picture
   onDrop(file, picture) {
     this.setState({image: this.state.image.concat(picture)});
-    document.body.style.backgroundImage = "url('https://anotherangle.eu/wp-content/uploads/2017/03/Geisel-Library-South-2048.jpg')";
+    document.getElementById('container').style.backgroundImage = "url('https://anotherangle.eu/wp-content/uploads/2017/03/Geisel-Library-South-2048.jpg')";
   }
 
   render() {
     return(
       // Render a form on the front-end to allow users to register and create their own profiles
-      <div className="container">
+      <div className="container" id="container">
         <div className="center">
         <form className="register-form">
           <h1><a href="/login" className="site-title">Triton Market</a></h1>
