e091bdc70d52867a32a891ee9c3fe028011f23b4
ishpatel <xishp117x@gmail.com>
2018-12-03 00:33:33 -0800

    ishpatel: Updated with new project/db connection to Firebase

--- src/components/Fire/fire.js
+++ src/components/Fire/fire.js
@@ -4,6 +4,18 @@
 
 import firebase from 'firebase';
 
+// New DB reference because we almost hit the limit for the last one
+var config = {
+    apiKey: "AIzaSyAWQIHQswUxj1U3G_K_ZrctVNEURVw88V0",
+    authDomain: "fast-v2-82035.firebaseapp.com",
+    databaseURL: "https://fast-v2-82035.firebaseio.com",
+    projectId: "fast-v2-82035",
+    storageBucket: "fast-v2-82035.appspot.com",
+    messagingSenderId: "909068737826"
+  };
+
+// Old DB reference in case new one breaks more stuff
+/*
 var config = {
   apiKey: "AIzaSyAMVKcugugokMLjDfp4N8KVqsEpvnzcqwU",
   authDomain: "fast-8cfa5.firebaseapp.com",
@@ -12,6 +24,7 @@ var config = {
   storageBucket: "fast-8cfa5.appspot.com",
   messagingSenderId: "413458791050"
 };
+*/
 const fire = firebase.initializeApp(config);
 // Export the database as "fire"
 export default fire;
