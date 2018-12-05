898a4752dc344590771a5a08d749897819f47a5c
Alex Z <alexz1012@yahoo.com>
2018-12-05 15:43:38 -0800

    Alex: Fix messages not sending properly

--- src/components/Messages/MessageSidebar/MessageSidebarButton/message_sidebar_button.js
+++ src/components/Messages/MessageSidebar/MessageSidebarButton/message_sidebar_button.js
@@ -5,7 +5,6 @@ export class MessageSidebarButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {active: this.props.active}
-    console.log(this.props.userID, this.props.listing['Buyer_ID'], this.props.listing['Seller_Name'], this.props);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -21,6 +20,7 @@ export class MessageSidebarButton extends React.Component {
         <div className="conversation-button-right">
           <div className="conversation-title">{this.props.title}</div>
           <div>{this.props.userID === this.props.listing['Buyer_ID'] ? this.props.listing['Seller_Name'] : this.props.listing['Buyer_Name']}</div>
+          <div>{this.props.listing['Is_Transaction_Log'] ? 'Transaction Complete' : null}</div>
         </div>
       </button>
     );
